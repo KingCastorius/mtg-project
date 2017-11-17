25d8599a5b7a4e932c558dd7dff7f9f061caaec5
KingCastorius <malizakzorz@gmail.com>
2017-11-16 18:06:08 -0800

    broken login

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -4,8 +4,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
 import Styles from '../styles'
+let key = 0
 
 class HomePage extends React.Component{
+  state = {
+    cardName: '',
+    returnedCards: []
+  }
+
+  getCards(e) {
+    e.preventDefault()
+    axios.get('https://api.magicthegathering.io/search/cards?q=' + this.state.cardName.then((res) => {
+      let cards = res.data.items.map(item => <li key={key++}>{item.mtg_url}</li>)
+      this.setState({returnedCards: cards})
+    }))
+  }
+
   render() {
     return(
       <div style={Styles.bgColor}>
@@ -16,7 +30,21 @@ class HomePage extends React.Component{
             </Col>
           </Row>
         </Container>
+          <form onSubmit={(e) => this.getCards(e)}>
+            <input
+              name="cardName"
+              type="text"
+              placeholder="card name"
+              value={this.state.cardName}
+              onChange={(e) => this.setState({[e.target.name]: e.target.value})} />
+            <button type="submit">Submit</button>
+          </form>
+
+          <ul>
+            {this.state.returnedCards}
+          </ul>
       </div>
+
     )
   }
 }
