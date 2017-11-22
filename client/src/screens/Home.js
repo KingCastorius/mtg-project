8fcf078f06115b9342861dd3f910a90b1a1939d1
KingCastorius <malizakzorz@gmail.com>
2017-11-21 20:10:14 -0800

    added card name and image pull from sdk

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from 'reactstrap';
-import { Container, Row, Col } from 'reactstrap';
+import {Grid, Row, Cell} from 'react-inline-grid';
 import Styles from '../styles'
 let key = 0
 const mtg = require('mtgsdk')
@@ -18,45 +18,57 @@ class HomePage extends React.Component{
     this.setState({[e.target.name]: e.target.value})
   }
 
-  getCards() {
-    mtg.card.all({ name: {e}, pageSize: 1 }).on('data', card => {
-      
+  getCards(e) {
+    e.preventDefault();
+    mtg.card.where({ name: this.state.cardName }).then((result) => {
+      let returnedCards = result.map(card =>
+        <div style={Styles.arial}>
+          <h4>{card.name}</h4>
+          <img src={card.imageUrl} />
+        </div>
+      );
+      this.setState({returnedCards: returnedCards});
     })
   }
 
   render() {
     return(
       <div style={Styles.bgColor}>
-        <Container>
+        <Grid>
           <Row>
-            <Col>
+            <Cell is="desktop-12" style={Styles.arial}>
               <h1 style={Styles.listColor}>MTG Collection Gallery</h1>
-            </Col>
+            </Cell>
           </Row>
-        </Container>
-          <form onSubmit={(e) => this.getCards(e)}>
-            <Row>
-              <input
-                name="cardName"
-                type="text"
-                placeholder="card name"
-                value={this.state.cardName}
-                onChange={(e) => this.setValue(e)}
-              />
-              <select>
-                <option value="name">Name</option>
-                <option value="cmc">Converted Mana Cost</option>
-                <option value="colors">Color</option>
-                <option value="type">Type</option>
-                <option value="subtypes">Subtypes</option>
-                <option value="power">Power</option>
-                <option value="toughness">Toughness</option>
-              </select>
-            </Row>
-            <Row>
-              <button type="submit" >Submit</button>
-            </Row>
-          </form>
+        </Grid>
+
+        <Grid>
+          <Row>
+            <Cell is="desktop-12" style={Styles.arial} >
+              <form onSubmit={(e) => this.getCards(e)}>
+                <input
+                  name="cardName"
+                  type="text"
+                  placeholder="card name"
+                  value={this.state.cardName}
+                  onChange={(e) => this.setValue(e)}
+                />
+                {/* <select>
+                  <option value="name">Name</option>
+                  <option value="cmc">Converted Mana Cost</option>
+                  <option value="colors">Color</option>
+                  <option value="type">Type</option>
+                  <option value="subtypes">Subtypes</option>
+                  <option value="power">Power</option>
+                  <option value="toughness">Toughness</option>
+                </select> */}
+                <div>
+                  <button type="submit" >Submit</button>
+                </div>
+              </form>
+            </Cell>
+          </Row>
+        </Grid>
 
           <ul>
             {this.state.returnedCards}
